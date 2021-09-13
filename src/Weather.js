8fcbc45e506f8502d71abc150e4e28878cb4e567
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-09-13 12:49:08 +0100

    Installed Axios, added API integration and updated CSS.

--- src/Weather.js
+++ src/Weather.js
@@ -1,31 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input type="search" placeholder="Enter a City name" className="form-control" />
-          </div>
-          <div className="col-3">
-            <input type= "submit" className="btn btn-primary" value="Search" />
-          </div>
-        </div>      
-      </form>
-      <h1>Kathmandu, NP</h1>
-      <h2>30°C</h2>
-      <ul className="small-text">
-        <li>Clear Sky | Friday</li>
+export default function Weather(props) {
 
-      </ul>
-      <ul className="more-weather-info">
-        <li className="humidity">Humidity: 79%</li>
-        <li className="date-time">10/09/2021 | 12:32</li>
-        <li>Wind: 6.26km/h</li>
-      </ul>
+  const [weatherData, setWeatherData] = useState({ ready: false });
 
-    </div>
-  )
+  function submitResult(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      city: response.data.name,
+      country: response.data.sys.country,
+      temperature:response.data.main.temp,
+      description: response.data.weather[0].description,
+      wind: 12,
+      humidity: response.data.main.humidity,
+      
+    })
+  }
+
+  
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input type="search" placeholder="Enter a City name" className="form-control" />
+            </div>
+            <div className="col-3">
+              <input type="submit" className="btn btn-primary" value="Search" />
+            </div>
+          </div>
+        </form>
+        <h1>{weatherData.city}, {weatherData.country}</h1>
+        <h2>{Math.round(weatherData.temperature)}°C</h2>
+        <ul className="small-text">
+          <li className ="text-capitalize">{weatherData.description} | 10/09/2021 </li>
+        </ul>
+        <ul className="more-weather-info">
+          <li className="humidity">Humidity: {Math.round(weatherData.humidity)}%</li>
+          <li className="date-time">Friday | 12:32</li>
+          <li>Wind: {weatherData.wind} km/h</li>
+        </ul>
+        <div className="switch">
+          <div className="btn_group">
+            <input type="submit" className="Hourly" value="Hourly" />
+            <div className="btn_bg"></div>
+            <input type="submit" className="Daily" value="Daily" />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    const apikey = "ba753d969dccd2973e89444d00d45191";
+    let city = "Kathmandu";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+    axios.get(apiUrl).then(submitResult);
+    return ("Loading...");
+  }
+
+ 
 }
