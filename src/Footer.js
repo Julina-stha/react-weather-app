80600acd24d0b05bc9d0e363bb7b229504df542e
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-09-22 18:15:35 +0100

    made some changes

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <small>
       <a href="https://github.com/Julina-stha/react-weather-app" target="_blank" rel="noreferrer">Open-source code</a>
-      <span> by Julina Shrestha. Design inspired by </span> <a href="https://www.behance.net/gallery/58673211/Weather-App" target="_blank" rel="noreferrer">Marina Schneider</a> 
+      <span> by Julina Shrestha.</span> 
     </small>
   );
 }
