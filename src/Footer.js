7b4f3972f39d13915a240179d704a18c520685e2
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-09-28 14:23:01 +0100

    Updated the Footer component and CSS styling

--- src/Footer.js
+++ src/Footer.js
@@ -4,8 +4,7 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <small>
-      <a href="https://github.com/Julina-stha/react-weather-app" target="_blank" rel="noreferrer">Open-source code</a>
-      <span> by Julina Shrestha.</span> 
+      Coded by <a href="https://www.linkedin.com/in/julina-shrestha-58a89a109/">Julina Shrestha.</a> It is <a href="https://github.com/Julina-stha/react-weather-app" target="_blank" rel="noreferrer">open-sourced on<img src="https://img.icons8.com/material-outlined/24/000000/github.png" alt="GitHub"/></a> and <a href="https://www.netlify.com/">hosted on Netlify</a>.
     </small>
   );
 }
