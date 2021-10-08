bd9cf4bb8d9105c0d9bff2ea9795141f9bbfe8a4
Julina-stha <80582692+Julina-stha@users.noreply.github.com>
2021-10-08 17:48:45 +0100

    Added the WeatherData component

--- src/TempButton.js
+++ src/TempButton.js
@@ -1,14 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import WeatherData from "./WeatherData";
 import "./Weather.css";
 
 export default function TempButton(props) {
-  const { onUnitChange } = props;
-  return (
-    <div className="col-3">
-      <div className="btn-group btn-group-mb-2">
-        <button type="button" className="btn btn btn-primary" id="cel" onClick={() => onUnitChange('metric')} >°C</button><span className="break">|</span>
-        <button type="button" className="btn btn btn-primary" id="fah" onClick={() => onUnitChange('imperial')} >°F</button>
+  const [unit, setUnit] = useState("metric")
+
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    setUnit("imperial");
+  }
+
+    function convertToCelsius(event) {
+    event.preventDefault();
+    setUnit("metric");
+  }
+
+  if (unit === "metric") {
+    return (
+      <div>
+        <div className="col-3">
+          <div className="btn-group btn-group-mb-2">
+            <button type="button" className="btn btn btn-primary" id="cel" onClick={convertToCelsius}>°C</button><span className="break">|</span>
+            <button type="button" className="btn btn btn-primary" id="fah"  onClick={convertToFahrenheit} >°F</button>
+          </div>
+        </div>
+        <h1>{props.data.city}, {props.data.country}</h1>
+        <h2>{Math.round(props.data.temperature)}°C</h2>
+        <WeatherData data={props.data} />
       </div>
-    </div>
-  )
+    )
+  } else {
+    let celsius = props.data.temperature
+    let fahrenheit = Math.round((celsius * 1.8) + 32);
+    return (
+      <div>
+        <div className="col-3">
+          <div className="btn-group btn-group-mb-2">
+            <button type="button" className="btn btn btn-primary" id="cel" onClick={convertToCelsius}>°C</button><span className="break">|</span>
+            <button type="button" className="btn btn btn-primary" id="fah" onClick={convertToFahrenheit} >°F</button>
+          </div>
+        </div>
+        <h1>{props.data.city}, {props.data.country}</h1>
+        <h2>{fahrenheit}°F</h2>
+        <WeatherData data={props.data} />
+      </div>
+    );
+  }
 }
